e517dc17ce5ba4a021089d4f7329bf879569c041
mutotu <muttalip.aydemir@gmail.com>
2022-02-24 23:56:32 -0800

    setting up

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+# this dot over here . right before import mean current folder
+from . import  views
+
+urlspattern = [
+    path("", views.starting_page, name="starting-page"),
+    path("posts", views.posts, name="posts-page"),
+    path("posts/<slug:slug>", views.post_detail, name="post-detail-page") #/posts/my-first-post     => slug is a unique identifier
+]
