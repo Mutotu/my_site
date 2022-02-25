1198650fae8939f3d80708f8e3ecedc6b687e8d9
mutotu <muttalip.aydemir@gmail.com>
2022-02-25 11:13:31 -0800

    testing css

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 # this dot over here . right before import mean current folder
 from . import  views
 
-urlspattern = [
+urlpatterns = [
     path("", views.starting_page, name="starting-page"),
     path("posts", views.posts, name="posts-page"),
     path("posts/<slug:slug>", views.post_detail, name="post-detail-page") #/posts/my-first-post     => slug is a unique identifier
